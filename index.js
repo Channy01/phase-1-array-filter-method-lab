3225616855647f3ee0f6661bcf3db80a443796e6
ChantiaKindle <chantia.kindle0123@gmail.com>
2022-04-10 22:42:33 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution here
+function findMatching(driverRoster, someStringThings) {
+  function cb(e) {
+    return e.toLowerCase() === someStringThings.toLowerCase();
+  }
+  const driverNames = driverRoster.filter(cb);
+  return driverNames;
+}
+
+// This function takes an array of drivers' names and a string as arguments for querying the array, and returns all drivers whose names begin with the provided letters.
+
+function fuzzyMatch(driverRoster, someStringThings) {
+  const trueMatch = driverRoster.filter(function cb(e) {
+    if (e.startsWith(someStringThings)) {
+      return e;
+    }
+  });
+  return trueMatch;
+}
+
+function matchName(driverRosterObj, someStringThings) {
+  function itsAMatch(record) {
+    if (record.name === someStringThings) {
+      return record;
+    }
+  }
+  const myDriver = driverRosterObj.filter(itsAMatch);
+  return myDriver;
+}
